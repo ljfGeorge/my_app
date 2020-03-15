09754abb00ecfddd33a7363f416f676d60f94af0
ljfGeorge <44706898+ljfGeorge@users.noreply.github.com>
2020-03-15 21:44:19 +0800

    Added Tailwind.
    Added Tailwind to Create React Application.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles.css' // <- change './index.css' to './styles.css'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
