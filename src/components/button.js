13945689fcff66c48ae6baf5ec9335e4a4d9f0c5
ljfGeorge <44706898+ljfGeorge@users.noreply.github.com>
2020-03-16 21:37:14 +0800

    test commit

--- src/components/button.js
+++ src/components/button.js
@@ -4,14 +4,14 @@ import React from "react";
 export default function Button({ children, ...buttonProps }) {
     return (
         <React.Fragment>
-            <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com"></input>
+            <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" placeholder="jane@example.com"></input>
             <button
                 className="px-2 py-1 rounded-lg bg-green-400 text-green-800 text-xl font-light uppercase shadow-md hover:shadow-lg"
                 {...buttonProps}
             >
                 {children}
             </button>
-        </React.Fragment>
+        </React.Fragment >
     );
 
 }
