82b4300f6a6381e6338afffe6a7c4a386d8955b3
ljfGeorge <44706898+ljfGeorge@users.noreply.github.com>
2020-03-16 21:48:48 +0800

    Updates

---
+++ precache-manifest.571ebcf5c83eb12339220080dc48cd73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f359dedfd9fc2b03bccb012446e082",
+    "url": "/my_app/index.html"
+  },
+  {
+    "revision": "2da1aefa8217d10c05b6",
+    "url": "/my_app/static/css/main.9fad0e8c.chunk.css"
+  },
+  {
+    "revision": "dcd81cf98267c26a071e",
+    "url": "/my_app/static/js/2.57bc27af.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my_app/static/js/2.57bc27af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da1aefa8217d10c05b6",
+    "url": "/my_app/static/js/main.78441851.chunk.js"
+  },
+  {
+    "revision": "5354c00b2dd505c68b96",
+    "url": "/my_app/static/js/runtime-main.694a2002.js"
+  }
+]);
